23b4576b61bb8909f686d12b5c011e531de1862f
Alexandr Kusmarov <Kusmarov_a@mail.ru>
2019-04-18 13:55:58 +0300

    Created @Entity classes

---
+++ src/main/java/com/gradulation/vote/domain/Dishes.java
@@ -0,0 +1,59 @@
+package com.gradulation.vote.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Dishes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String dishname;
+    private Integer price;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+//    @JoinColumn(name = "")
+    private Restaurant restaurant;
+
+    public Dishes() {
+    }
+
+    public Dishes(String dishname, Integer price, Restaurant restaurant) {
+        this.dishname = dishname;
+        this.price = price;
+        this.restaurant = restaurant;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDishname() {
+        return dishname;
+    }
+
+    public void setDishname(String dishname) {
+        this.dishname = dishname;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+}
---
+++ src/main/java/com/gradulation/vote/domain/Restaurant.java
@@ -0,0 +1,50 @@
+package com.gradulation.vote.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String restname;
+
+    @OneToMany(fetch = FetchType.EAGER)
+//    @JoinColumn(name = "")
+    private Set<Dishes> dishes;
+
+    public Restaurant() {
+    }
+
+    public Restaurant(String restname, Set<Dishes> dishes) {
+        this.restname = restname;
+        this.dishes = dishes;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRestname() {
+        return restname;
+    }
+
+    public void setRestname(String restname) {
+        this.restname = restname;
+    }
+
+    public Set<Dishes> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(Set<Dishes> dishes) {
+        this.dishes = dishes;
+    }
+}
